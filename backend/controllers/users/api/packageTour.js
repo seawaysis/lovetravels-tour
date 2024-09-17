2e1691a01ceff38b520db8f628324df05f3bc030
Wiwat <seawaysia@gmail.com>
2024-09-17 17:30:27 +0700

    hotfix query packagetour and path picture in backend

--- backend/controllers/users/api/packageTour.js
+++ backend/controllers/users/api/packageTour.js
@@ -6,10 +6,10 @@ const searchPackage = async (req,res) => {
     const body = req.body
     const searchSQL = {
         searchWord : '',
-        searchDate : '',
-        searchAmount : ''
+        searchDate : ''
     }
     const arrSearch= []
+    arrSearch.push(body.amount)
     if(body.search){
         searchSQL.searchWord = '(p.package_name LIKE ? OR p.description LIKE ?) AND'
         arrSearch.push('%'+body.search+'%')
@@ -22,14 +22,21 @@ const searchPackage = async (req,res) => {
     }else{
         searchSQL.searchDate = 'p.start_date <= ?';
     }
-    arrSearch.push(body.amount)
     arrSearch.push('active')
-    searchSQL.searchAmount = 'p.max_amount > ?';
-    const queryText = `SELECT p.package_name,p.description,p.company_name,p.price_person,p.discount,g.pic_path,g.package_id FROM packageTour AS p LEFT JOIN gallery AS g ON p.package_id = g.package_id WHERE ${searchSQL.searchWord} ${searchSQL.searchDate} AND ${searchSQL.searchAmount} AND p.status = ?`;
-    const result = await sequelize.query(queryText, {
+    const queryText = `SELECT p.package_name,p.description,p.company_name,p.max_amount,p.price_person,p.discount,g.pic_path,g.package_id,(IFNULL(r.sum_amount,0) + ?) AS sum_amount FROM packageTour AS p LEFT JOIN gallery AS g ON p.package_id = g.package_id LEFT JOIN (SELECT package_id,SUM(amount) AS sum_amount FROM reservation WHERE status = 'confirmed' GROUP BY package_id) AS r ON p.package_id = r.package_id WHERE ${searchSQL.searchWord} ${searchSQL.searchDate} AND p.status = ?`;
+    const temp = await sequelize.query(queryText, {
         replacements: arrSearch,
         type: QueryTypes.SELECT,
     });
+    let result = []
+    const fullHost = `${req.protocol}://${req.get('host')}/package_tour/`
+    temp.forEach((v,k) => {
+        if(v.sum_amount <= v.max_amount){
+            temp[k].pic_path = fullHost+''+temp[k].pic_path
+            result.push(temp[k])
+        }
+    });
+    console.log(result)
     res.status(200).send(result);
 }
 const detailDescription = async (req,res) => {
@@ -39,7 +46,7 @@ const detailDescription = async (req,res) => {
         replacements: arrSearch,
         type: QueryTypes.SELECT,
     });
-    res.status(200).send({message: result});
+    res.status(200).send(result);
 }
 module.exports={
     searchPackage
