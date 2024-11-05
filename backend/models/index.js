e96e69da2b496f36702784c9975886a662a20cd4
SeawaysiS <seawaydev@gmail.com>
2024-11-05 23:45:42 +0700

    connect postgres online

--- backend/models/index.js
+++ backend/models/index.js
@@ -14,7 +14,14 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  //sequelize = new Sequelize(config.database, config.username, config.password, config); // for Mysql and Postgres in docker
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
 }
 
 fs
