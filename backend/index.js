a01cc029e29ce10de0ba5966e3267b740fed428a
SeawaysiS <seawaydev@gmail.com>
2024-11-07 22:38:00 +0700

    update vercel

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,52 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
-const path = require('path')
+// const path = require('path')
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
-const adminRoutes = require('./routes/admins/admin');
-const userRoutes = require('./routes/users/user');
-const agentRoutes = require('./routes/agents/agent');
-const db = require('./models');
+// const adminRoutes = require('./routes/admins/admin');
+// const userRoutes = require('./routes/users/user');
+// const agentRoutes = require('./routes/agents/agent');
+// const db = require('./models');
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
-const resetDB = {force:false};
-db.sequelize.sync(resetDB).then(() => {
-    db.extradb(resetDB)
+// const resetDB = {force:false};
+// db.sequelize.sync(resetDB).then(() => {
+//     db.extradb(resetDB)
     
-    app.use('/admin',adminRoutes);
-    app.use('/user',userRoutes);
-    app.use('/agent',agentRoutes);
+//     app.use('/admin',adminRoutes);
+//     app.use('/user',userRoutes);
+//     app.use('/agent',agentRoutes);
 
-    app.use('/bg_search',express.static(path.join(__dirname,'/src/images/bgSearch')));
-    app.use('/package_tour',express.static(path.join(__dirname,'/src/images/packageTour')));
-    app.use('/qr_code',express.static(path.join(__dirname,'/src/images/qrcode')));
-    app.use('/e_slip',express.static(path.join(__dirname,'/src/images/eSlip')));
+//     app.use('/bg_search',express.static(path.join(__dirname,'/src/images/bgSearch')));
+//     app.use('/package_tour',express.static(path.join(__dirname,'/src/images/packageTour')));
+//     app.use('/qr_code',express.static(path.join(__dirname,'/src/images/qrcode')));
+//     app.use('/e_slip',express.static(path.join(__dirname,'/src/images/eSlip')));
 
-    app.get('/bg_search/:filename', (req, res) => {
-        res.sendFile(path.join(__dirname, "/src/images/bgSearch/"+req.params.filename));
-    });
-    app.get('/package_tour/:filename', (req, res) => {
-        res.sendFile(path.join(__dirname, "/src/images/packageTour/"+req.params.filename));
-    });
-    app.get('/qr_code/:filename', (req, res) => {
-        res.sendFile(path.join(__dirname, "/src/images/qrcode/"+req.params.filename));
-    });
-    app.get('/e_slip/:filename', (req, res) => {
-        res.sendFile(path.join(__dirname, "/src/images/eSlip/"+req.params.filename));
-    });
+//     app.get('/bg_search/:filename', (req, res) => {
+//         res.sendFile(path.join(__dirname, "/src/images/bgSearch/"+req.params.filename));
+//     });
+//     app.get('/package_tour/:filename', (req, res) => {
+//         res.sendFile(path.join(__dirname, "/src/images/packageTour/"+req.params.filename));
+//     });
+//     app.get('/qr_code/:filename', (req, res) => {
+//         res.sendFile(path.join(__dirname, "/src/images/qrcode/"+req.params.filename));
+//     });
+//     app.get('/e_slip/:filename', (req, res) => {
+//         res.sendFile(path.join(__dirname, "/src/images/eSlip/"+req.params.filename));
+//     });
 
+//     app.listen(process.env.PORT_BE,() =>{
+//         console.log('Server on port ',process.env.PORT_BE)
+//     });
+// });
+
+    app.get("/", (req, res) => res.send("Express on Vercel"));
     app.listen(process.env.PORT_BE,() =>{
         console.log('Server on port ',process.env.PORT_BE)
     });
-});
 
-module.exports = app;
+// module.exports = app;
